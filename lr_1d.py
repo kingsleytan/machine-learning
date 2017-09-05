ed2cb51af28e5af75dd45962269347186fc742fa
Kingsley <sengking.tan@gmail.com>
2017-09-05 16:27:24 +0800

    calculate predicted Y, r-squared

--- lr_1d.py
+++ lr_1d.py
@@ -17,3 +17,21 @@ Y = np.array(Y)
 # let's plot the data to see what it looks like
 plt.scatter(X, Y)
 plt.show()
+
+denominator = X.dot(X) - X.mean() * X.sum()
+a = ( X.dot(Y) - Y.mean()*X.sum() ) / denominator
+b = ( Y.mean() * X.dot(X) - X.mean() * X.dot(Y) ) / denominator
+
+# let's calculate the predicted Y
+Yhat = a*X + b
+
+# let's plot everything together to make sure it worked
+plt.scatter(X, Y)
+plt.plot(X, Yhat)
+plt.show()
+
+# determine how good the model is by computing the r-squared
+d1 = Y - Yhat
+d2 = Y - Y.mean()
+r2 = 1 - d1.dot(d1) / d2.dot(d2)
+print("the r-squared is:", r2)
