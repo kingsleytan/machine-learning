5e20fa79d2309299e02b2b4d50e39fee9b795e48
Kingsley <sengking.tan@gmail.com>
2017-09-05 16:19:11 +0800

    imported csv data, plot on scatter graph

---
+++ lr_1d.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load the data
+X = []
+Y = []
+for line in open('data_1d.csv'):
+    x, y = line.split(',')
+    X.append(float(x))
+    Y.append(float(y))
+
+# let's turn X and Y into numpy arrays since that will be useful later
+X = np.array(X)
+Y = np.array(Y)
+
+
+# let's plot the data to see what it looks like
+plt.scatter(X, Y)
+plt.show()
